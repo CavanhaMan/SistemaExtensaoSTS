dd0388452f7c9fe67ae172afec7b9cc4ed51b41a
CavanhaMan <cavanhaman@yahoo.com.br>
2018-09-02 20:33:23 -0300

    Adicionando campo idade

---
+++ src/main/java/br/edu/iftm/extensao/TesteMaster.java
@@ -0,0 +1,13 @@
+/*Rodrigo CavanhaMan*/
+package br.edu.iftm.extensao;
+
+public class TesteMaster {
+
+	private int idade;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
